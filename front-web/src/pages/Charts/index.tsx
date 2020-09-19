1acec6b91e9ef620ef09840153d9e518ac91d2bf
Caio Silva <caio.silvacm@gmail.com>
2020-09-18 23:52:40 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://projeto-devsds1-caio-silva.herokuapp.com';
 
 const Charts = () => {
     const [BarChartData, setBarChartData] = useState<BarChartData[]>([]);
